4c68de19be08e3c8557613b92c6806468d8de6a0
superfroggman <49985901+superfroggman@users.noreply.github.com>
2020-06-18 21:40:07 +0200

    added collision with paddles and started with user input

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,8 +2,12 @@ package sample;
 
 import javafx.animation.AnimationTimer;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
 public class Controller {
@@ -45,6 +49,12 @@ public class Controller {
 
     long prevTime = 0;
 
+    boolean keyLUP = false;
+    boolean keyLDown = false;
+
+    boolean keyRUP = false;
+    boolean keyRDown = false;
+
     public void roundSetup() {
         //Setup game container
         gameGrid.setStyle("-fx-background-color: #C0C0C0;");
@@ -70,7 +80,6 @@ public class Controller {
 
         paddleLY = gameH / 2 - paddleLH / 2;
         paddleRY = gameH / 2 - paddleRH / 2;
-
 
 
         //Set initial ball speed
@@ -106,37 +115,108 @@ public class Controller {
     public void onNewFrame(long dT) {
 
 
-        if (ballX + ballW >= gameW) {
-            ballX = gameW - ballW;
-            ballXspd *= -1;
-        }
 
-        if (ballX <= 0) {
-            ballX = 0;
-            ballXspd *= -1;
-        }
-
-        if (ballY + ballH >= gameH) {
-            ballY = gameH - ballH;
-            ballYspd *= -1;
-        }
-
-        if (ballY <= 0) {
-            ballY = 0;
-            ballYspd *= -1;
-        }
 
         ballX += ballXspd * dT;
         ballY += ballYspd * dT;
 
-        //System.out.println("ballX: " + ballX);
-        //System.out.println("new frame, dT: " + dT);
-
         ball.setX(ballX);
         ball.setY(ballY);
 
+
         paddleL.setY(paddleLY);
         paddleR.setY(paddleRY);
+
+
+    }
+
+    private void checkWallCollision(){
+        //Collision checking between ball and wall
+        if (ballX + ballW >= gameW) {
+            ballX = gameW - ballW;
+            ballXspd *= -1;
+        }
+        if (ballX <= 0) {
+            ballX = 0;
+            ballXspd *= -1;
+        }
+        if (ballY + ballH >= gameH) {
+            ballY = gameH - ballH;
+            ballYspd *= -1;
+        }
+        if (ballY <= 0) {
+            ballY = 0;
+            ballYspd *= -1;
+        }
+    }
+
+    private void checkPaddleCollision(){
+        paddleL.setFill(Color.CHOCOLATE);
+        paddleR.setFill(Color.CHOCOLATE);
+
+        //check collision with left paddle
+        if (colliding(ballX, ballW, paddleL.getX(), paddleLW)) {
+            if (colliding(ballY, ballH, paddleLY, paddleLH)) {
+                if (ballXspd < 0) ballXspd *= -1;
+                paddleL.setFill(Color.CRIMSON);
+            }
+        }
+
+        //check collision with right paddle
+        if (colliding(ballX, ballW, paddleR.getX(), paddleRW)) {
+            if (colliding(ballY, ballH, paddleRY, paddleRH)) {
+                if (ballXspd > 0) ballXspd *= -1;
+                paddleR.setFill(Color.CRIMSON);
+            }
+        }
+    }
+
+    private void checkInputs(){
+        Main.mainScene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent event) {
+                System.out.println("pressed: " + event.getCode());
+                switch (event.getCode()) {
+                    case W:
+                        keyLUP = true;
+                        break;
+                    case S:
+                        keyLDown = true;
+                        break;
+                    case UP:
+                        keyRUP = true;
+                        break;
+                    case DOWN:
+                        keyRDown = true;
+                        break;
+                }
+            }
+        });
+
+        Main.mainScene.setOnKeyReleased(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent event) {
+                System.out.println("released: " + event.getCode());
+                switch (event.getCode()) {
+                    case W:
+                        keyLUP = false;
+                        break;
+                    case S:
+                        keyLDown = false;
+                        break;
+                    case UP:
+                        keyRUP = false;
+                        break;
+                    case DOWN:
+                        keyRDown = false;
+                        break;
+                }
+            }
+        });
+    }
+
+    private boolean colliding(double obj1Pos, double obj1Size, double obj2Pos, double obj2Size) {
+        return (obj1Pos >= obj2Pos && obj1Pos <= obj2Pos + obj2Size) || (obj1Pos + obj1Size >= obj2Pos && obj1Pos + obj1Size <= obj2Pos + obj2Size);
     }
 
     /**
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -8,11 +8,14 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
+    public static Scene mainScene;
+
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 1280, 720));
+        mainScene = new Scene(root, 1280, 720);
+        primaryStage.setScene(mainScene);
         primaryStage.show();
     }
 
